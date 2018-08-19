1a4badd8d229c39e2cfb8886ab3f73c668dd2e82
Zzobi_conf <presquerien@openmailbox.org>
2018-08-19 14:05:14 +0200

    alpha one

---
+++ src/drums.h
@@ -0,0 +1,45 @@
+#ifndef DRUMS_H
+#define DRUMS_H
+#include <Audio.h>
+
+class Clap {
+public:
+  Clap(){};
+  AudioSynthNoiseWhite     noise;
+  AudioFilterStateVariable bpf;
+  AudioEffectEnvelope      env;
+
+private:
+};
+
+class Kick {
+public:
+  AudioSynthWaveformModulated  sine;
+  AudioFilterStateVariable     lpf;
+  AudioEffectEnvelope          env;
+private:
+};
+
+class HH {
+public:
+  HH(){};
+  AudioSynthWaveform        square[6];
+  AudioMixer4               mixer[3];
+  AudioFilterStateVariable  bpf;
+  AudioFilterStateVariable  hpf;
+  AudioEffectEnvelope       oh_env;
+  AudioEffectEnvelope       ch_env;
+
+private:
+  int attack = 20;
+  int decay = 10;
+  int sustain = 0.3;
+  int oh_release = 1000;
+  int ch_release = 270;
+  int bpf_freq = 10000;
+  int hpf_freq = 7000;
+  int base_freq = 40;
+  float ratio[6] = {2, 3, 4.16, 5.43, 6.79, 8.21};
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <Arduino.h>
+#include <Audio.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <SerialFlash.h>
+#include "drums.h"
+
+struct IO {
+  AudioOutputAnalog           output;
+  AudioOutputUSB              usb;
+};
+
+IO    io;
+Clap  clap;
+Kick  kick;
+HH    hihat;
+
+void doCons();
 
 void setup() {
-    // put your setup code here, to run once:
+  doCons(); //make audioconnections
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+}
+
+void doCons() {
+  //Clap
+  AudioConnection          patchCord1(clap.noise, 0, clap.bpf, 0);
+  AudioConnection          patchCord2(clap.bpf, 1, clap.env, 0);
+  //Kick
+  //HH
+  //mixers
 }
